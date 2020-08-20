90977b154d8c7767d9568383bba0a656023edd08
Alexander Römer <alex@roemer.design>
2020-08-20 12:19:53 +0200

    Initial Commit

---
+++ WiggleAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WiggleAnimation
+//
+//  Created by Alexander Römer on 20.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WiggleAnimation/WiggleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiggleAnimationApp.swift
+//  WiggleAnimation
+//
+//  Created by Alexander Römer on 20.08.20.
+//
+
+import SwiftUI
+
+@main
+struct WiggleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
